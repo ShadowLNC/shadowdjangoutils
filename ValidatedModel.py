aca30f23549fa106d458ee5c703aa2a27c07020f
Scott Stevens <go.scott.mac@gmail.com>
2017-04-17 00:38:54 +0930

    PEP8 and disable carry-over errors by default. (ValidatedModel.py)
    - Previously, save() would accumulate errors across repeated calls.
    - This functionality is now prevented by default, as repeated save() calls would accumulate errors, even if they had been fixed.
    - Specify the extra_errs parameter to be equal to the object's errs dictionary in order to restore previous behaviour.
    - (So that the errs dictionary is always defined, it's now set in a newly-added constructor.)

--- ValidatedModel.py
+++ ValidatedModel.py
@@ -2,23 +2,28 @@ from django.db import models
 from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 from django.utils.translation import ugettext_lazy as _
 
+
 # Custom model validation in the clean() method; this is the boilerplate code.
 class ValidatedModel(models.Model):
-    def save(self, *args, **kwargs):
-        # Try to clean and check errors.
-        # In case errs already exists, do not overwrite. See addErr().
-        if not hasattr(self, 'errs'): self.errs = {}
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.errs = {}  # For addErr() calls without calling save().
+
+    def save(self, extra_errs=None, *args, **kwargs):
+        # Try to clean, then check errors. We aallow pre-existing errors to be
+        # raised. Specify extra_errs=<self_object>.errs in order to carry over
+        # previous errors when saving, instead of clearing the slate (default).
+        if extra_errs is None:
+            extra_errs = {}
+        self.errs = extra_errs
         self.full_clean()
 
         # After cleaning, either raise the errors or else continue with save.
-        if self.errs != {}: raise ValidationError(self.errs)
-        super(ValidatedModel, self).save(*args, **kwargs)
+        if self.errs != {}:
+            raise ValidationError(self.errs)
+        super().save(*args, **kwargs)
 
     def addErr(self, text, field=NON_FIELD_ERRORS, code=None, params=None):
-        # In case someone calls this method without save(), we create the errs
-        # dict, up to them to call save() eventually.
-        if not hasattr(self, 'errs'): self.errs = {}
-
         # This isn't about raising individual ValidationErrors; you can do that
         # elsewhere if you want; it's a single line of code.
         error = ValidationError(_(text), code=code, params=params)
